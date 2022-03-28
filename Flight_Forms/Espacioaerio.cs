cb65b6d7e40ed40a351e43afea3753645c3bc564
Sergiogd112 <sergio.gomezdamas@gmail.com>
2022-03-28 10:10:04 +0200

    update

--- Flight_Forms/Espacioaerio.cs
+++ Flight_Forms/Espacioaerio.cs
@@ -20,7 +20,6 @@ namespace Flight_Forms
         double dist;
 
         PictureBox[] plane;
-        Graphics[] distanciaSeguridadArea;
         FlightPlan plan;
 
         int butt = 1;
@@ -29,7 +28,6 @@ namespace Flight_Forms
         {
             this.lista = l;
             this.plane = new PictureBox[lista.GetMaxLen()];
-            this.distanciaSeguridadArea = new Graphics[lista.GetMaxLen()];
             this.ciclo = c;
             SetStyle(ControlStyles.SupportsTransparentBackColor, true);
 
@@ -41,27 +39,29 @@ namespace Flight_Forms
         {
             for (int i = 0; i < lista.GetLen(); i++)
             {
-                plan = lista.GetFlightAtIndex(i);
+                plan = this.lista.GetFlightAtIndex(i);
                 plane[i] = new PictureBox();
                 plane[i].Location = new Point(Convert.ToInt32(plan.GetInitialPosition().GetX()), Convert.ToInt32(plan.GetInitialPosition().GetY()));
                 plane[i].ClientSize = new Size(40, 40);
                 plane[i].SizeMode = PictureBoxSizeMode.StretchImage;
                 plane[i].BackColor = Color.Transparent;
                 plane[i].Image = new Bitmap(@"..\..\Properties\avion.gif");
-                /*                Color newColor = Color.FromArgb(128, Color.Green);
-                                SolidBrush myBrush = new SolidBrush(newColor);
-                                distanciaSeguridadArea[i] = panel2.CreateGraphics();
-                                distanciaSeguridadArea[i].FillEllipse(myBrush, new Rectangle(Convert.ToInt32(plan.GetInitialPosition().GetX())- Convert.ToInt32(lista.GetDistanciaSeguridad()),
-                                    Convert.ToInt32(plan.GetInitialPosition().GetY())- Convert.ToInt32(lista.GetDistanciaSeguridad()),
-                                    Convert.ToInt32(plan.GetInitialPosition().GetX())+ Convert.ToInt32(lista.GetDistanciaSeguridad()), 
-                                    Convert.ToInt32(plan.GetInitialPosition().GetY())+ Convert.ToInt32(lista.GetDistanciaSeguridad())));
-                                myBrush.Dispose();
-                                distanciaSeguridadArea[i].Dispose();
+
+                /*
+                plane[i].DoubleClick += delegate (object s, EventArgs events)
+                {
+                    clickFlight(plan);
+                };
                 */
+
+
+
                 panel2.Controls.Add(plane[i]);
             }
-            //this.Close();  
+
         }
+
+        private void clickFli
 
 
         private void panel2_MouseMove(object sender, MouseEventArgs e)
@@ -172,9 +172,8 @@ namespace Flight_Forms
                 }
             }
             if (distanciaInferior())
-            {
                 this.Close();
-            }
+
         }
         private bool distanciaInferior()
         {
@@ -191,10 +190,10 @@ namespace Flight_Forms
 
         private void button1_Click(object sender, EventArgs e)
         {
-            plane0 = lista.GetFlightAtIndex(0).GetCurrentPosition();
-            plane1 = lista.GetFlightAtIndex(1).GetCurrentPosition();
-            int distplane0 = Convert.ToInt32(lista.GetFlightAtIndex(0).GetFinalPosition().Distancia(plane0));
-            int distplane1 = Convert.ToInt32(lista.GetFlightAtIndex(1).GetFinalPosition().Distancia(plane1));
+            plane0 = lista.GetFlightAtIndex(0).GetCurrentPosition(); // la posició actual de l'avió en la posició 0
+            plane1 = lista.GetFlightAtIndex(1).GetCurrentPosition(); // la posició actual de l'avió en la posició 1
+            int distplane0 = Convert.ToInt32(lista.GetFlightAtIndex(0).GetFinalPosition().Distancia(plane0)); // la distància entre la posició final i actual de l'avió en la posició 0
+            int distplane1 = Convert.ToInt32(lista.GetFlightAtIndex(1).GetFinalPosition().Distancia(plane1)); // la distància entre la posició final i actual de l'avió en la posició 1
 
             for (int i = 1; distplane0 != 0 && distplane1 != 0; i++)
             {
